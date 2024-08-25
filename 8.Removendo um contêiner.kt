2ac9dced7d81a5020b6f406e507e28538f5689e4
Rodrigo Luís Coelli <rodrigocoelli@hotmail.com>
2024-08-25 20:40:35 -0300

    Create 8.Removendo um contêiner.kt

---
+++ 8.Removendo um contêiner.kt
@@ -0,0 +1,3 @@
+Com o contêiner parado, podemos removê-lo utilizando o comando abaixo. Em seguida podemos também executar o comando para remover a imagem baixada.
+
+docker container rm ID_DO_CONTAINER
