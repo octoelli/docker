21cd0c1e68d55b09cb2b9f404a2e17f03ce6b553
Rodrigo Luís Coelli <rodrigocoelli@hotmail.com>
2024-08-25 20:35:50 -0300

    Create Docker para Desenvolvedores.kt

---
+++ Docker para Desenvolvedores.kt
@@ -0,0 +1,11 @@
+Embora o Docker seja voltado para infraestrutura, um dos seus grandes usos também são ambientes de desenvolvimento.
+
+Hoje trabalhamos com diversos bancos de dados, diversas tecnologias e serviços, isto tudo gera um acúmulo de instalações muito grandes.
+
+Imagina instalar o SQL Server, MySQL e MongoDb, todos na sua máquina, sendo executados como serviços do Windows. É complicado.
+
+Neste caso, o que podemos fazer é utilizar imagens prontas de máquinas com SQL Server, MySQL e Mongo e simplesmente executá-las quando quisermos.
+
+Quer criar um projeto com SQL Server? Só fazer o download da imagem oficial dele para Docker, executar e pronto, temos o serviço rodando.
+
+O Docker funciona para Mac, Windows e Linux, e no caso do Windows, recomendamos utilizar a versão 10, pois ela tem suporte à subsistemas Linux, que são a base do Docker.
