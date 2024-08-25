b9b3d6324b2a89253f07ff5dd9f863bbb428c923
Rodrigo Luís Coelli <rodrigocoelli@hotmail.com>
2024-08-25 20:41:02 -0300

    Create 9.Publicando uma API.kt

---
+++ 9.Publicando uma API.kt
@@ -0,0 +1,8 @@
+Chegou a hora do desafio final, vamos publicar uma API criada utilizando Node/JavaScript em um contêiner e torná-la acessível pelo nosso Browser.
+
+Para executar esta parte do artigo você precisará do Git e do Visual Studio Code instalados em sua máquina.
+
+Abra um terminal e navegue para uma pasta segura, sem caracteres especiais ou com caminho muito longo e execute o seguinte comando.
+
+git clone https://github.com/andrebaltieri/docker-sample-api.git
+Este comando fará o download do código fonte de uma API de exemplo que está no nosso GitHub, gerando assim uma nova pasta chamada docker-sample-api.
