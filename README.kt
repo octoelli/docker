f5340c162ccc615984036b981e500690078b54c7
Rodrigo Luís Coelli <rodrigocoelli@hotmail.com>
2024-08-25 20:34:33 -0300

    Update and rename README.md to README.kt

---
+++ README.kt
@@ -0,0 +1,19 @@
+Docker - Instalação, Configuração e Primeiros Passos
+O Docker é uma forma de abstração da nossa infraestrutura que nos beneficia tanto em tempo de desenvolvimento quanto em produção.
+
+Índice
+O que é o Docker?
+Docker para Desenvolvedores
+Requisitos
+Versões do Docker
+Criando a Conta
+Instalando o Docker
+Executando o Docker
+Testando a Instalação
+Listando as Imagens
+Listando os Contêineres
+Publicando uma API
+Criando uma Imagem
+Rodando o Contêiner
+Acessando os Logs
+WSL 2 (Somente Windows)
