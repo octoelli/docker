a16ea5d11bb699d358380bfa323a4bfd61482e60
Rodrigo Luís Coelli <rodrigocoelli@hotmail.com>
2024-08-25 20:36:38 -0300

    Create Executando o Docker.kt

---
+++ Executando o Docker.kt
@@ -0,0 +1,15 @@
+Após reiniciar o computador, inicie o Docker caso o mesmo não tenha sido iniciado. O nome da aplicação no sistema ficará como Docker mesmo.
+
+Você saberá que ele está em execução caso o ícone do Docker (uma baleia) esteja próximo ao relógio (System Tray).
+
+No primeiro acesso você será indagado para autenticar utilizando seu Docker ID criado previamente neste artigo.
+
+Recomendamos fortemente que faça o login, e caso tenha perdido esta primeira tela, basta clicar novamente no ícone do Docker (baleia) e selecionar a opção Sign In.
+
+Isto é tudo que precisamos para ter o Docker rodando em nossa máquina. Em adicional recomendamos que clique novamente no ícone do Docker e vá até a opção Settings.
+
+Na aba General, desmarque a opção Start Docker for Desktop when you Login.
+
+Isto fará com que o Docker não seja iniciado junto com o sistema operacional, tornando a inicialização mais rápida.
+
+Caso tenha uma máquina mais poderosa e queira deixar esta opção marcada, fique à vontade.
