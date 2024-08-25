cc0d7cf87ac4d7cf1bb20ae9ec7f461332510cde
Rodrigo Luís Coelli <rodrigocoelli@hotmail.com>
2024-08-25 20:40:11 -0300

    Create 7.Iniciando e parando um contêiner.kt

---
+++ 7.Iniciando e parando um contêiner.kt
@@ -0,0 +1,7 @@
+Para executar ou para um contêiner, podemos usar o comando start/stop.
+
+docker container start ID_DO_CONTAINER
+docker container stop ID_DO_CONTAINER
+Para ver a lista completa do que podemos fazer com um contêiner basta executar o comando abaixo.
+
+docker container --help
